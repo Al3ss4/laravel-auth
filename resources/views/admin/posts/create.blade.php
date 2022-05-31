119579dfec35740ec37e686543683940ccfed356
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-31 16:29:17 +0200

    add create and show

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,4 +1,50 @@
+@extends('layouts.dashboard')
 
+@section('content')
+
+<div class="container">
+
+
+
+
+
+
+
+    <form action="{{route('admin.posts.store')}}" method="post">
+        @csrf
+        <div class="mb-3">
+          <label for="titolo" class="form-label">Titolo</label>
+          {{-- old title consente di immagazzinare il valore precedentente --}}
+          <input type="text" class="form-control
+          @error('title')
+          is-invalid
+          @enderror"
+          id="titolo" name="title" value="{{old('title')}}">
+          @error('title')
+          <div class="alert alert-warning">{{$message}}</div>
+          @enderror
+        </div>
+
+        <div class="mb-3">
+          <label for="descrizione" class="form-label">Descrizione</label>
+
+          <textarea
+            name="content" id="descrizione" cols="30" rows="10" class="form-control
+            @error('content')
+            is-invalid
+            @enderror">
+            {{old('content')}}
+          </textarea>
+          @error('content')
+          <div class="alert alert-warning">{{$message}}</div>
+          @enderror
+
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
 
 
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,2 +1,26 @@
+@extends('layouts.dashboard')
+@section('content')
 
+<div class="container">
+    <div class="card">
+        <h2 class="p-3">In dettaglio</h2>
+        <div class="card-header">
+            {{$post->id}}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"> {{ $post->title }}</h5>
+          <p class="card-text"> {{$post->content}}</p>
+          <p class="card-text"> {{$post->slug}}</p>
+        </div>
+
+        <div class="p-3">
+            <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna indietro</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection()
 
